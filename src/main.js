c0abac1000ba3e3737e5af12c707a60dedf26d66
= <=>
2023-10-24 22:59:41 +0700

    fix conflict main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,7 @@ import "./assets/styles/style.css"
 import "./assets/styles/global.scss"
 import "./assets/styles/header.scss"
 import "./assets/styles/home.scss"
-<<<<<<< HEAD
 import "./assets/styles/footer.scss"
-=======
 import "./assets/styles/login.scss"
->>>>>>> f5e4dabb (commit 1- login)
 
 createApp(App).mount('#app')
