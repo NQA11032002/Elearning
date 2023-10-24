08a444643401a5b906ac2ae43b413e121b1441c0
NQA11032002 <kawasakisga@gmail.com>
2023-10-24 21:13:55 +0700

    commit 2 - complete designed for footer

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import "./assets/styles/style.css"
 import "./assets/styles/global.scss"
 import "./assets/styles/header.scss"
+import "./assets/styles/home.scss"
 import "./assets/styles/footer.scss"
 
 createApp(App).mount('#app')
