46b190b087e344f1b9d857b420ca28d033d5a279
HoQuangDat <hoquangdat001@gmail.com>
2023-12-12 17:34:09 +0700

    UI RegisteredCourse

---
+++ src/components/client/RegisteredCourses.vue
@@ -0,0 +1,114 @@
+<script>
+import SidebarmemberScreen from './SidebarmemberScreen.vue';
+
+export default {
+    components: {
+        SidebarmemberScreen,
+    },
+};
+</script>
+<template>
+    <div class="w-full flex my-5 max-sm:flex-col">
+        <sidebarmember-screen></sidebarmember-screen>
+        <div class="ml-6 w-4/5 max-sm:mt-4 bg-white max-sm:w-full max-sm:ml-0">
+            <div class="p-4">
+                <p class="text-xl font-semibold">Khóa học đã đăng ký</p>
+                <span class="text-gray-500 text-sm"></span>
+            </div>
+
+            <div class="p-4 mt-4">
+                <div class="">
+                    <div class="flex justify-between border-b border-gray-200 max-sm:flex-col">
+                        <span class="font-medium text-blue-700 text-base max-sm:text-sm">Chuyên gia: Hồ Quang Đạt</span>
+                        <div class="max-sm:hidden">
+                            <i class="fa-solid fa-chevron-left text-sm"></i>
+                            <i class="fa-solid fa-chevron-right ml-2 text-sm"></i>
+                        </div>
+                    </div>
+                    <div class="flex gap-2 max-sm:flex-col">
+                        <div class="w-full ">
+                            <div class="full shadow-lg p-6 bg-white rounded-md">
+                                <img class="w-full h-40 object-cover" src="../../assets/images/client/courses/1.png" alt="">
+                                <span class="font-semibold">Khóa học React cơ bản dành cho người mới bắt đầu</span>
+                                <div class="w-full text-right mt-4">
+                                    <span class="bg-orange-600 p-1 px-2 text-white text-sm">Đang thịnh hành <i class="fa-solid fa-fire-flame-curved text-orange-300"></i></span>
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full ">
+                            <div class="full shadow-lg p-6 bg-white rounded-md">
+                                <img class="w-full h-40 object-cover" src="../../assets/images/client/courses/1.png" alt="">
+                                <span class="font-semibold">Khóa học React cơ bản dành cho người mới bắt đầu</span>
+                                <div class="w-full text-right mt-4">
+                                    <span class="bg-orange-600 p-1 px-2 text-white text-sm">Đang thịnh hành <i class="fa-solid fa-fire-flame-curved text-orange-300"></i></span>
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full">
+                            <div class="full shadow-lg p-6 bg-white rounded-md">
+                                <img class="w-full h-40 object-cover" src="../../assets/images/client/courses/1.png" alt="">
+                                <span class="font-semibold">Khóa học React cơ bản dành cho người mới bắt đầu</span>
+                                <div class="w-full text-right mt-4">
+                                    <span class="bg-orange-600 p-1 px-2 text-white text-sm">Đang thịnh hành <i class="fa-solid fa-fire-flame-curved text-orange-300"></i></span>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div> 
+            </div>
+
+            <div class="p-4 mt-4">
+                <div class="">
+                    <div class="flex justify-between border-b border-gray-200 max-sm:flex-col">
+                        <span class="font-medium text-blue-700 text-base max-sm:text-sm">Chuyên gia: Hồ Quang Đạt</span>
+                        <div class="max-sm:hidden">
+                            <i class="fa-solid fa-chevron-left text-sm"></i>
+                            <i class="fa-solid fa-chevron-right ml-2 text-sm"></i>
+                        </div>
+                    </div>
+                    <div class="flex gap-2 max-sm:flex-col">
+                        <div class="w-full ">
+                            <div class="full shadow-lg p-6 bg-white rounded-md">
+                                <img class="w-full h-40 object-cover" src="../../assets/images/client/courses/1.png" alt="">
+                                <span class="font-semibold">Khóa học React cơ bản dành cho người mới bắt đầu</span>
+                                <div class="w-full text-right mt-4">
+                                    <span class="bg-orange-600 p-1 px-2 text-white text-sm">Đang thịnh hành <i class="fa-solid fa-fire-flame-curved text-orange-300"></i></span>
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full ">
+                            <div class="full shadow-lg p-6 bg-white rounded-md">
+                                <img class="w-full h-40 object-cover" src="../../assets/images/client/courses/1.png" alt="">
+                                <span class="font-semibold">Khóa học React cơ bản dành cho người mới bắt đầu</span>
+                                <div class="w-full text-right mt-4">
+                                    <span class="bg-orange-600 p-1 px-2 text-white text-sm">Đang thịnh hành <i class="fa-solid fa-fire-flame-curved text-orange-300"></i></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full">
+                            <div class="full shadow-lg p-6 bg-white rounded-md">
+                                <img class="w-full h-40 object-cover" src="../../assets/images/client/courses/1.png" alt="">
+                                <span class="font-semibold">Khóa học React cơ bản dành cho người mới bắt đầu</span>
+                                <div class="w-full text-right mt-4">
+                                    <span class="bg-orange-600 p-1 px-2 text-white text-sm">Đang thịnh hành <i class="fa-solid fa-fire-flame-curved text-orange-300"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div> 
+            </div>
+
+            
+        </div>
+    </div>
+</template>
