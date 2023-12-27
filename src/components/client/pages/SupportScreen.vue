2556d107d77a69809faa7626de4194eb91e81299
HoQuangDat <hoquangdat001@gmail.com>
2023-12-27 18:01:13 +0700

    UI tranning, support

--- src/components/client/pages/SupportScreen.vue
+++ src/components/client/pages/SupportScreen.vue
@@ -1,5 +1,84 @@
 <template>
-  <div>
-    <p>Support</p>
+  <div class="w-2/3 m-auto max-sm:w-full max-sm:px-5 sm:px-3 lg:px-0 mt-14">
+    <div class="w-full my-5 max-sm:flex-col">
+      <div class="flex">
+        <div class="w-1/2 flex flex-col">
+          <h1 class="w-full text-center font-semibold text-3xl border-b border-gray-300 p-4">Về chúng tôi</h1>
+            <div class="flex flex-col leading-7 p-4">
+              <span>E-learning Academylink là một nền tảng giáo dục trực tuyến nổi bật, 
+                nơi bạn có thể trải nghiệm các khóa học chất lượng cao được thiết kế bởi những chuyên gia hàng đầu trong ngành. 
+                Chúng tôi cam kết mang đến cho học viên những trải nghiệm học tập linh hoạt, hiệu quả và tiện ích.
+              </span>
+                <span>
+                  1.Đội Ngũ Giảng Viên Xuất Sắc:
+    
+                  Các khóa học của chúng tôi được phát triển bởi đội ngũ giáo viên có kinh nghiệm và chuyên gia trong lĩnh vực, đảm bảo nội dung chất lượng và thực tế.
+                </span>
+                <span>
+                2. Đa Dạng Về Chủ Đề:
+                
+                Với sự đa dạng về chủ đề, từ kỹ năng mềm đến khoa học công nghệ, chúng tôi cung cấp nhiều lựa chọn học tập để đáp ứng mọi nhu cầu và quan tâm.
+                Học Trực Tuyến Linh Hoạt:
+                
+                Học viên có thể truy cập nền tảng của chúng tôi mọi nơi, mọi lúc, tận hưởng sự linh hoạt và thuận tiện trong quá trình học.
+               </span>
+                
+            </div>
+         
+        </div>
+        <div class="w-1/2 p-4">
+          <img class="w-full h-96 object-cover" src="https://img.freepik.com/free-vector/e-learning-icons-flat_1284-3950.jpg?w=2000" alt="">
+        </div>
+      </div>
+
+      <div class="flex my-10">
+        <div class="w-1/2 p-4">
+          <img class="w-full h-96 object-cover" src="https://img.freepik.com/free-vector/students-using-e-learning-platform-video-laptop-graduation-cap-online-education-platform-e-learning-platform-online-teaching-concept_335657-795.jpg" alt="">
+        </div>
+        <div class="w-1/2 flex flex-col">
+          <h1 class="w-full text-center font-semibold text-3xl border-b border-gray-300 p-4">Liên hệ</h1>
+            <div class="flex flex-col leading-7 p-4">
+              <span>Chúng tôi luôn sẵn lòng lắng nghe và giải đáp mọi thắc mắc của bạn. Đừng ngần ngại liên hệ với chúng tôi để có thêm thông tin hoặc hỗ trợ. 
+                Dưới đây là thông tin liên hệ của chúng tôi:
+              </span>
+                <span>
+                  <b>Địa Chỉ:</b>
+                  E-learning Academylink
+                  Đường ABC, Quận XYZ
+                  Thành phố, Quốc gia
+                </span>
+                <span>
+                  <b>Số Điện Thoại:</b>
+                  (+84) 123 456 789
+               </span>
+               <span>
+                <b>Email:</b>
+                info@academylink.com
+             </span>
+            </div>
+        </div>      
+      </div>
+
+      <div class="flex">
+        <div class="w-1/2 flex flex-col">
+          <h1 class="w-full text-center font-semibold text-3xl border-b border-gray-300 p-4">Gửi thông tin cần hỗ trợ</h1>
+            <div class="flex flex-col leading-7 p-4 ">
+              <input class="outline-none border border-gray-400 mb-4 rounded p-2" placeholder="Nhập tiêu đề" type="text">
+              <input class="outline-none border border-gray-400 mb-4 rounded p-2" placeholder="Họ tên" type="text">
+              <input class="outline-none border border-gray-400 mb-4 rounded p-2" placeholder="Email" type="text">
+              <textarea class="outline-none border border-gray-400 mb-4 rounded p-2" placeholder="Nội dung cần hỗ trợ" name="" id="" cols="30" rows="10"></textarea>
+              <button class="w-full bg-blue-900 p-2 text-white rounded p-2">Gửi ngay</button>
+            </div>
+         
+        </div>
+
+        <div class="w-1/2 p-4">
+          <iframe class="w-full h-96" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2063.6927116478373!2d109.21704141060668!3d13.758982400864014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x316f6cebf252c49f%3A0xa83caa291737172f!2zVHLGsOG7nW5nIMSQ4bqhaSBI4buNYyBRdXkgTmjGoW4!5e0!3m2!1svi!2s!4v1703673734822!5m2!1svi!2s" width="600" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        
+      </div>
+      
+    </div>
   </div>
+
 </template>
