40376ac94e896d613b0599dd0ea84dda3d984fcd
HoQuangDat <hoquangdat001@gmail.com>
2023-12-27 18:01:13 +0700

    UI tranning, support

--- src/components/client/pages/TrainingScreen.vue
+++ src/components/client/pages/TrainingScreen.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <p>training</p>
+  <div class="w-2/3 m-auto max-sm:w-full max-sm:px-5 sm:px-3 lg:px-0 mt-14">
+    <div class="w-full my-5 max-sm:flex-col">
+      <div class="w-full">
+        <div class="relative">
+          <img class="w-full h-96" src="https://www.dynamicpixel.co.in/assets/img/home/E-Learning-Courses.jpg" alt="">
+          <div class=" w-1/3 h-48 bg-black absolute text-center top-10 right-72 p-4   opacity-50">
+            <span class="text-white">Nội dung khóa học</span>
+
+          </div>
+        </div>
+        
+      </div>
+    </div>
   </div>
+
 </template>
